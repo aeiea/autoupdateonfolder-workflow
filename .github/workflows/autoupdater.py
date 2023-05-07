fcb2231382651365d5711130c00c3d599a0486da
aeiea <118844761+aeiea@users.noreply.github.com>
2023-05-06 17:00:19 -0700

    this might work

---
+++ .github/workflows/autoupdater.py
@@ -0,0 +1,25 @@
+# 🚢 IMPORTS
+import os, subprocess
+
+# ⚙️ CONFIG
+repo_to_update = "https://github.com/aeiea/autoupdateonfolder-workflow.git" # put your repo's github thingy
+folder_to_check_for_updates = "test" # checks the folder test for updates
+file_to_update = "listoffiles.txt" # list of files in the directory test
+
+# 🐍 CODE
+try:
+      gh_key = os.environ["API_KEY"]
+except:
+      print("Hey! you might of forgotten your github API key!")
+print('''
+       ,            .    ,       
+ _.. .-+- _ . .._  _| _.-+- _ ._.
+(_](_| | (_)(_|[_)(_](_] | (/,[  
+               |                 
+          by @aeiea
+ used on {your username}/{repo}
+''')
+subprocess.run("git clone " + repo_to_update + " thisfolderisgoingtobedeleted")
+files = [f for f in os.listdir("/thisfolderisgoingtobedeleted/" + folder_to_check_for_updates) if os.path.isfile(os.path.join("/thisfolderisgoingtobedeleted/" + folder_to_check_for_updates, f))]
+print(files)
+os.rmdir("/thisfolderisgoingtobedeleted")
